324ded244d5269a0634eff83ea3202cd2a4c3c9c
Peter Forret <peter@forret.com>
2020-10-14 23:16:08 +0200

    add more comments

--- install.sh
+++ install.sh
@@ -1,55 +1,42 @@
 #!/usr/bin/env bash
 
-die(){
+die() {
   echo "!! $1 " >&2
   echo "!! -----------------------------" >&2
   exit 1
 }
 
-git version > /dev/null 2>&1 || die "git is not installed on this machine"
+## stop if basher is already installed
 [[ -d "$HOME/.basher" ]] && die "basher is already installed on [$HOME/.basher]"
 
 ## install the scripts on ~/.basher
+git version >/dev/null 2>&1 || die "git is not installed on this machine"
 git clone https://github.com/basherpm/basher.git ~/.basher
 
 ## now check what shell is running
 shell_type=$(basename "$SHELL")
 case "$shell_type" in
-bash)
-    startup_script="$HOME/.bashrc"
-    startup_type=simple
-  ;;
-
-zsh)
-    startup_script="$HOME/.zshrc"
-    startup_type=simple
-  ;;
-
-sh)
-    startup_script="$HOME/.profile"
-    startup_type=simple
-  ;;
-
-fish)
-    startup_script="$HOME/.config/fish/config.fish"
-    startup_type=fish
-  ;;
-
-*)
-    startup_script=""
-    startup_type=""
+bash)  startup_type="simple" ; startup_script="$HOME/.bashrc" ;;
+zsh)   startup_type="simple" ; startup_script="$HOME/.zshrc"  ;;
+sh)    startup_type="simple" ; startup_script="$HOME/.profile";;
+fish)  startup_type="fish"   ; startup_script="$HOME/.config/fish/config.fish"  ;;
+*)     startup_type="?"      ; startup_script="" ;   ;;
 esac
 
+## startup script should exist already
 [[ -n "$startup_script" && ! -f "$startup_script" ]] && die "startup script [$startup_script] does not exist"
 
+## basher_keyword will allow us to remove the lines upon uninstall
 basher_keyword="basher5ea843"
-if [[ "$startup_type" == "simple" ]] ; then
+
+## now add the basher initialisation lines to the user's startup script
+if [[ "$startup_type" == "simple" ]]; then
   (
     echo "export PATH=\"$HOME/.basher/bin:$PATH\"   ##$basher_keyword"
     # shellcheck disable=SC2086
     echo "eval \"$(basher init - $shell_type)\"             ##$basher_keyword"
-  ) >> "$startup_script"
-elif [[ "$startup_type" == "fish" ]] ; then
+  ) >>"$startup_script"
+elif [[ "$startup_type" == "fish" ]]; then
   (
     echo "if test -d ~/.basher          ##$basher_keyword"
     echo "  set basher ~/.basher/bin    ##$basher_keyword"
@@ -57,7 +44,10 @@ elif [[ "$startup_type" == "fish" ]] ; then
     # shellcheck disable=SC2154
     echo "set -gx PATH $basher $PATH    ##$basher_keyword"
     echo "status --is-interactive; and . (basher init - $shell_type | psub)    ##$basher_keyword"
-  ) >> "$startup_script"
+  ) >>"$startup_script"
+else
+  die "unknown shell [$shell_type] - can't initialise"
 fi
 
+## script is finished
 echo "basher is installed - open a new terminal window to start using it"
