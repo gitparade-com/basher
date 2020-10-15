e67cf6cf9a95ad6ca426a22bf483101c221dc0f7
Peter Forret <peter@forret.com>
2020-10-15 19:47:00 +0200

    fix install/uninstall script

--- install.sh
+++ install.sh
@@ -9,12 +9,16 @@ die() {
 ## stop if basher is already installed
 [[ -d "$HOME/.basher" ]] && die "basher is already installed on [$HOME/.basher]"
 
-## install the scripts on ~/.basher
+## stop if git in not installed
 git version >/dev/null 2>&1 || die "git is not installed on this machine"
-git clone https://github.com/basherpm/basher.git ~/.basher
+
+## install the scripts on ~/.basher
+echo ". download basher code to ~/.basher"
+git clone https://github.com/basherpm/basher.git ~/.basher 2> /dev/null
 
 ## now check what shell is running
 shell_type=$(basename "$SHELL")
+echo ". detected shell type: $shell_type"
 case "$shell_type" in
 bash)  startup_type="simple" ; startup_script="$HOME/.bashrc" ;;
 zsh)   startup_type="simple" ; startup_script="$HOME/.zshrc"  ;;
@@ -30,11 +34,12 @@ esac
 basher_keyword="basher5ea843"
 
 ## now add the basher initialisation lines to the user's startup script
+echo ". add basher initialisation to [$startup_script]"
 if [[ "$startup_type" == "simple" ]]; then
   (
     echo "export PATH=\"\$HOME/.basher/bin:\$PATH\"   ##$basher_keyword"
     # shellcheck disable=SC2086
-    echo "eval \"$(basher init - $shell_type)\"             ##$basher_keyword"
+    echo "eval \"\$(basher init - $shell_type)\"             ##$basher_keyword"
   ) >>"$startup_script"
 elif [[ "$startup_type" == "fish" ]]; then
   (
